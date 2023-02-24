7283d4fa65c36c48dc35d31042768fe55ea56c9f
Daniel Wilson <wilsonhardrock@gmail.com>
2023-02-24 16:48:17 -0500

    Refactor to use stat functions

--- skirmish-report-stats/skirmish-report-stats.js
+++ skirmish-report-stats/skirmish-report-stats.js
@@ -41,102 +41,114 @@ const ansHullKeys = [
 - Ranks of participating players and what team they were on (to offset rank bias)
 */
 function handleFileUpload(file) {
-    return file.text().then(handleFileContent).catch(alert).then((xmlDoc) => {
-        const resolver = xmlDoc.createNSResolver(xmlDoc);
-
-        const teams = [];
-
-        const winningTeam = xmlDoc.evaluate("//WinningTeam", xmlDoc, resolver, XPathResult.STRING_TYPE).stringValue;
-        const teamReports = xmlDoc.evaluate("//TeamReportOfShipBattleReport", xmlDoc, resolver, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
-        let report = teamReports.iterateNext();
-        while(report) {
-            const teamID = xmlDoc.evaluate("./TeamID", report, resolver, XPathResult.STRING_TYPE).stringValue;
-            const players = xmlDoc.evaluate(".//PlayerName", report, resolver, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
-
-            const playersRet = [];
-
-            let player = players.iterateNext();
-            while(player) {
-                playersRet.push(player.textContent);
-
-                player = players.iterateNext();
-            }
-            let points = xmlDoc.evaluate(".//OriginalPointCost", report, resolver, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
-            let point = points.iterateNext();
-            let sum = 0;
-            while(point) {
-                sum += parseInt(point.textContent, 10);
-                point = points.iterateNext();
-            }
-            let ships = xmlDoc.evaluate(".//HullKey", report, resolver, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
-            let ship = ships.iterateNext();
-            let isOsp = false;
-            let isAns = false;
-            while(ship) {
-                if(ansHullKeys.includes(ship.textContent)) {
-                    isAns = true;
-                } else if (ospHullKeys.includes(ship.textContent)) {
-                    isOsp = true;
-                }
-                ship = ships.iterateNext();
-            }
-
-            let faction = "";
-            if (isOsp && isAns) {
-                faction = "OSP/ANS";
-            } else if (isOsp) {
-                faction = "OSP";
-            } else if (isAns) {
-                faction = "ANS";
-            }
-
-            const team = {
-                team: teamID,
-                players: playersRet,
-                points: sum,
-                faction: faction
-            }
-            teams.push(team);
-
-            report = teamReports.iterateNext();
-        }
-
-        const winningFaction = teams.find((t) => t.team === winningTeam).faction;
-
-        return { winner: `${winningTeam} - ${winningFaction}`, teams: teams, file: file.name };
-    }).catch(alert);
+    return file.text().then(handleFileContent).catch(alert).then((doc) => [file.name, doc]);
 }
 
-const teamCols = [];
-function renderStats(stats) {
+function isFaction(hullKeys) {
+    return (doc, teamReport) => {
+        const resolver = doc.createNSResolver(doc);
+    
+        let ships = doc.evaluate(".//HullKey", teamReport, resolver, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
+        let ship = ships.iterateNext();
+        while(ship) {
+            if(hullKeys.includes(ship.textContent)) {
+                return true;
+            }
+            ship = ships.iterateNext();
+        }
+        return false;
+    };
+}
+const isANS = isFaction(ansHullKeys);
+const isOSP = isFaction(ospHullKeys);
+
+function statReportName(docname) {
+    return docname;
+}
+
+function statWinningTeam(_, doc) {
+    const resolver = doc.createNSResolver(doc);
+    return doc.evaluate("//WinningTeam", doc, resolver, XPathResult.STRING_TYPE).stringValue;
+}
+
+function statPlayers(selectFn) {
+    return (_, doc) => {
+        const resolver = doc.createNSResolver(doc);
+        const teamReports = doc.evaluate("//TeamReportOfShipBattleReport", doc, resolver, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
+        let report = teamReports.iterateNext();
+        while(report) {
+            if (selectFn(doc, report)) {
+                const players = doc.evaluate(".//PlayerName", report, resolver, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
+
+                const playersRet = [];
+
+                let player = players.iterateNext();
+                while(player) {
+                    playersRet.push(player.textContent);
+
+                    player = players.iterateNext();
+                }
+
+                return playersRet.join(", ");
+            }
+            report = teamReports.iterateNext();
+        }
+        return "";
+    }
+}
+
+function statCombatPower(selectFn) {
+    return (_, doc) => {
+        const resolver = doc.createNSResolver(doc);
+        const teamReports = doc.evaluate("//TeamReportOfShipBattleReport", doc, resolver, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
+        let report = teamReports.iterateNext();
+        while(report) {
+            if (selectFn(doc, report)) {
+                let points = doc.evaluate(".//OriginalPointCost", report, resolver, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
+                let point = points.iterateNext();
+                let sum = 0;
+                while(point) {
+                    sum += parseInt(point.textContent, 10);
+                    point = points.iterateNext();
+                }
+                return sum.toLocaleString();
+            }
+            report = teamReports.iterateNext();
+        }
+        return "";
+    }
+}
+const statCols = {
+    "Report": statReportName,
+    "Winning Team": statWinningTeam,
+    "ANS Players": statPlayers(isANS),
+    "ANS Combat Power": statCombatPower(isANS),
+    "OSP Players": statPlayers(isOSP),
+    "OSP Combat Power": statCombatPower(isANS)
+};
+
+function renderRowStats(docs) {
     const statHead = document.getElementById("stat-head");
     const statTable = document.getElementById("stat-table");
+    statHead.innerHTML = "";
     statTable.innerHTML = "";
-    stats.forEach((stat) => {
+
+    Object.keys(statCols).forEach((statName) => {
+        const th = document.createElement("th");
+        th.textContent = statName;
+        statHead.appendChild(th);
+    })
+    docs.forEach(([docname, doc]) => {
         const tr = document.createElement("tr");
-        let cell = document.createElement("td");
-        cell.textContent = stat.file;
-        tr.appendChild(cell);
-        cell = document.createElement("td");
-        cell.textContent = stat.winner;
-        tr.appendChild(cell);
-        stat.teams.forEach((team) => {
-            if(!teamCols.includes(team.team)) {
-                teamCols.push(team.team);
-                let headcell = document.createElement("th");
-                headcell.textContent = team.team;
-                statHead.appendChild(headcell);
-                headcell = document.createElement("th");
-                headcell.textContent = "Points";
-                statHead.appendChild(headcell);
+        for (const stat in statCols) {
+            if (Object.hasOwnProperty.call(statCols, stat)) {
+                const statFn = statCols[stat];
+    
+                const td = document.createElement("td");
+                td.textContent = statFn(docname, doc);
+                tr.appendChild(td);
             }
-            cell = document.createElement("td");
-            cell.textContent = `${team.faction} - ${team.players.join(", ")}`;
-            tr.appendChild(cell);
-            cell = document.createElement("td");
-            cell.textContent = team.points.toString();
-            tr.appendChild(cell);
-        });
+        }
         statTable.appendChild(tr);
     });
 }
@@ -145,34 +157,34 @@ function ui() {
     const dragTarget = document.getElementById("drag-target");
     const fileInput = document.getElementById("report-file");
     fileInput.addEventListener("change", (e) => {
-        const statPromises = [];
+        const docPromises = [];
         if (fileInput.files) {
             [...fileInput.files].forEach((file, i) => {
                 if (file.name.endsWith(".xml")) {
-                    statPromises.push(handleFileUpload(file));
+                    docPromises.push(handleFileUpload(file));
                 }
             });
         }
-        Promise.all(statPromises).then((stats) => {
-            renderStats(stats);
+        Promise.all(docPromises).then((docs) => {
+            renderRowStats(docs);
         });
     });
 
     dragTarget.addEventListener("drop", (e) => {
         e.preventDefault();
         dragTarget.classList.remove("dragging");
-        const statPromises = [];
+        const docPromises = [];
 
         if (e.dataTransfer.files) {
             [...e.dataTransfer.files].forEach((file, i) => {
                 if (file.name.endsWith(".xml")) {
-                    statPromises.push(handleFileUpload(file));
+                    docPromises.push(handleFileUpload(file));
                 }
             });
         }
 
-        Promise.all(statPromises).then((stats) => {
-            renderStats(stats);
+        Promise.all(docPromises).then((docs) => {
+            renderRowStats(docs);
         });
     });
 
