b118da8b3071eed0c2ef50b9d528a1fbcd730357
Gesugao-san <gesugao.san@gmail.com>
2023-11-29 00:06:32 +0300

    Add self-injecting HTML head

--- js/head.js
+++ js/head.js
@@ -1,43 +1,39 @@
-let str = '<link rel="icon" type="image/png" href="./favicon.ico">\
-  <link rel="manifest" href="./minmaxtool.webmanifest" />\
-  <meta charset="utf-8">\
+
+const my_name = 'head.js';
+const str_to_inject = '<link rel="icon" type="image/png" href="./favicon.ico">\n\
+  <link rel="manifest" href="./minmaxtool.webmanifest" />\n\
+  <meta charset="utf-8">\n\
   <meta name="viewport" content="width=device-width, initial-scale=1.0">\
-'
-let parsedDoc = new DOMParser().parseFromString(str, 'text/html');
-let parsedDoc2 = parsedDoc.children[0];
-let parsedDoc3 = parsedDoc2.children[0];
-let parsedDoc4 = parsedDoc3.children; //.innerHTML
+';
 
-console.log('parsedDoc:', parsedDoc.innerHTML);
-console.log('parsedDoc2:', parsedDoc2.innerHTML);
-console.log('parsedDoc3:', parsedDoc3.innerHTML);
-console.log('parsedDoc4:', parsedDoc4.innerHTML);
-
-//document.head.appendChild(parsedDoc);
-//document.head.appendChild(parsedDoc2);
-//document.head.appendChild(parsedDoc3);
-
-let scripts = document.getElementsByTagName('script');
-let script = false; //scripts[scripts.length-1];
-for (let i = 0; i < scripts.length; i++) {
-  if (scripts[i].src.includes('head.js')) {
-    script = scripts[i];
+function getMyselfLegacy() {
+  let scripts = document.getElementsByTagName('script');
+  for (let i = 0; i < scripts.length; i++) {
+    if (scripts[i].src.includes(my_name)) {
+      return scripts[i];
+    }
   }
-}
-if (!script) {
-  //return
-  console.log('FAILED');
+  return false;
 }
 
-//document.head.insertAdjacentHTML('afterbegin', str);
-script.insertAdjacentHTML('beforebegin', str);
-script.remove();
+function injectHead(code) {
+  //let parsedDoc = new DOMParser().parseFromString(str, 'text/html');
+  let script = document.currentScript || false;
+  if (!script) {
+    console.warn('Graceful injecting HTML head method failed. Trying legacy one.');
+    script = getMyselfLegacy();
+  }
+  if (!script) {
+    err_msg = 'Failed to inject standart HTML head. Fatal error.';
+    console.error(err_msg);
+    return alert(err_msg);
+  }
+  //document.head.insertAdjacentHTML('afterbegin', str);
+  script.insertAdjacentHTML('beforebegin', code);
+  script.remove();
+  console.log('Head was injected successfully.');
+}
 
-//let div = document.Element('div');
-//div.innerHTML = 'Whatever content is going to be inserted';
-//script.parentNode.insertBefore(div, script);
+//if (typeof require !== 'undefined' && require.main === module) {}
 
-
-console.log('number of rows:', parsedDoc4.getElementById('viewport')); //rows.length
-
-console.log('textContent of the fourth td:',parsedDoc3.querySelectorAll('td')[3].textContent);
+injectHead(str_to_inject);
